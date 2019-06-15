8f7d0ac81a6ca654142916c86f3d8dfeb00099b6
hothman <houcemoo@gmail.com>
2019-06-15 07:21:34 +0100

    Add readstr module

---
+++ src/readstr.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Reads structure files: PDB, multi model PDB, DCD, XTC
+See LICENSE
+"""
+
+# Built-in/Generic Imports
+import os
+import sys
+
+
+# import other Libs
+
+# my modules
+
+
+__author__ = 'Houcemeddine Othman'
+__copyright__ = 'Copyright 2019, AYDA'
+__license__ = 'MIT License'
+__maintainer__ = 'Houcemeddine Othmqn'
+__email__ = 'houcemoo@gmail.com'
+__status__ = 'in progress'
+
+
+
+
+
